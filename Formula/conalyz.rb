d8207b64543610f14b723a95db359a6ab9240220
Sourav-Sonkar <ss12011999@gmail.com>
2026-06-01 19:31:44 +0530

    release: update SHA256 for v1.0.0 binary

--- Formula/conalyz.rb
+++ Formula/conalyz.rb
@@ -6,7 +6,7 @@ class Conalyz < Formula
   on_macos do
     on_arm do
       url "https://github.com/conalyz/homebrew-conalyz/releases/download/v1.0.0/conalyz"
-      sha256 "f96c5d96fcc5be379cbc7ae0ec23c306e2bfd579e851eca69b489e2b1853ad9f"
+      sha256 "b5a7fd84ba6c56da2975e06e3765ee382ee0e6f1345413fbce2dff46b9f6e8de"
     end
   end
 
